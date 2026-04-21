38ff0b0af7f29267cf4d910f9b99e9018701837e
val <val@DESKTOP-RSTCNEB>
2026-04-21 16:06:39 +0300

    LAB 7

---
+++ Student test/Test1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentTracker; 
+using System;
+using System.Linq;
+
+namespace StudentTracker.Tests
+{
+   
+    [TestClass]
+    public class TaskItemTests
+    {
+        [TestMethod]
+        public void MarkAsDone_ShouldChangeStatusToCompleted()
+        {
+           
+            var task = new TaskItem(1, 1, "Тестове завдання", "Опис", DateTime.Now.AddDays(1));
+
+            
+            task.MarkAsDone();
+
+           
+            Assert.AreEqual(TaskStatus.Completed, task.Status, "Статус не змінився на 'Виконано'.");
+        }
+    }
+
+    [TestClass]
+    public class TaskRepositoryTests
+    {
+        [TestMethod]
+        public void AddTask_ShouldSaveTaskToRepository()
+        {
+            // Arrange
+            var repo = new TaskRepository();
+            var task = new TaskItem(99, 1, "Тест для репозиторію", "", DateTime.Now.AddDays(2));
+
+            // Act
+            repo.Add(task);
+            var result = repo.GetById(99);
+
+            // Assert
+            Assert.IsNotNull(result, "Завдання не було додано до репозиторію.");
+            Assert.AreEqual("Тест для репозиторію", result.Title);
+
+            
+            repo.Remove(task);
+        }
+    }
+}
